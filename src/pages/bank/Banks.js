f0aad6d21d3447358c4b95b3bf6a88ec32e0618c
Nwife <ifeeamaeze@gmail.com>
2022-06-23 16:59:38 +0100

    implemented modal and form for each bank

--- src/pages/bank/Banks.js
+++ src/pages/bank/Banks.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 //styles
@@ -10,11 +11,27 @@ import search from '../../assets/search.svg'
 import { banklist } from './banklist';
 
 //components
+import BankModal from '../../components/bankmodal/BankModal';
 import GeneralLayout from '../../components/layout/GeneralLayout';
 import CondueetLogo from '../../components/condueet-logo/CondueetLogo';
 
 export default function Banks() {
   console.log("banklist>>>", banklist)
+  const [id, setId] = useState(null);
+  const [modal, setModal] = useState(false);
+
+  //open modal
+  const openModal = (index) => {
+    setId(index);
+    setModal(true)
+  }
+    
+  //close the modal
+  const handleClose = () => {
+    setModal(false)
+    console.log('aas')
+  }
+
   return (
     <GeneralLayout>
         <section className='banks'>
@@ -28,14 +45,15 @@ export default function Banks() {
                         <h1>Choose your bank</h1>
                         <div className="banks-list_wrap">
                             {banklist.map((bank, index) => (
-                                <Link to={`/banks/${index}`} className='banks-list_item' key={bank.id}>
+                                <div onClick={() => openModal(index)} className='banks-list_item' key={bank.id}>
                                     <img src={bank.logo} alt="logo" />
                                     <p>{bank.name}</p>
-                                </Link>
+                                </div>
                             ))}
                         </div>
                     </div>
             </div>
+            { modal && <BankModal handleClose={handleClose} id={id}/> }
         </section>
     </GeneralLayout>
    
--- src/pages/bank/banklist.js
+++ src/pages/bank/banklist.js
@@ -3,6 +3,6 @@ export const banklist = [
     { 'id': '999044', name: 'Access bank', logo: '/banklogos/access.svg', xlogo: '/banklogos/x-access.svg', pColor: '#F6BC78', sColor: '#EF821E' },
     { 'id': '999214', name: 'FCMB', logo: '/banklogos/FCMB.svg', xlogo: '/banklogos/x-FCMB.svg', pColor: '#AE72D2', sColor: '#5C2684' },
     { 'id': '999058', name: 'GTB', logo: '/banklogos/gtbank.svg', xlogo: '/banklogos/x-gtbank.svg', pColor: '#D29A72', sColor: '#DD4F04' },
-    { 'id': '999058', name: 'Kuda', logo: '/banklogos/kuda.svg', xlogo: '/banklogos/x-kuda.svg', pColor: '#AC90D1', sColor: '#40196C' },
+    { 'id': '999052', name: 'Kuda', logo: '/banklogos/kuda.svg', xlogo: '/banklogos/x-kuda.svg', pColor: '#AC90D1', sColor: '#40196C' },
     { 'id': '999215', name: 'Unity Bank', logo: '/banklogos/unity.svg', xlogo: '/banklogos/x-unity.svg', pColor: '#fff', sColor: '#82C447' },
 ];
